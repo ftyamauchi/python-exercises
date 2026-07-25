773cb8a5b3f95a9dac35695bfbd0c5c3d7493f14
Fabio Yamauchi <141425679+ftyamauchi@users.noreply.github.com>
2026-07-25 15:42:01 -0400

    Create ola.py

---
+++ ola.py
@@ -0,0 +1,7 @@
+def diga_ola():
+    nome = input('Digite seu nome: ')
+    print(f'Olá {nome}! Seja bem vindo(a).')
+    idade = int(input('Por favor me diga sua idade: '))
+    print(f'Perfeito {nome}. Você tem {idade} anos.')
+
+#diga_ola()
